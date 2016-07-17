a12c13d286f7c24f863e920455b6c106c1238666
Novikov Alexnde Mac <alexndr.novikov@gmail.com>
2016-07-17 17:04:19 +0400

    [init] added laravel application to automatize database different formats export

---
+++ database/migrations/2016_07_17_101521_create_products_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
